9be3889f11442359a7b18284d5f7dc2c34fc5d88
ayush <ayushkrsngh2003@gmail.com>
2024-11-30 23:25:11 +0530

    db_url update

--- database.py
+++ database.py
@@ -38,7 +38,7 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 # Construct database URL
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DB_URL")
 
 # DATABASE_URL = "sqlite:///./musicapp.db"
 # engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
